1b19c06c6196c74457a33b77cd1abf993a686fc7
Nag46214 <97864157+Nag46214@users.noreply.github.com>
2022-09-12 23:03:06 +0530

    Update PatternFive.java

--- PatternFive.java
+++ PatternFive.java
@@ -1,17 +1,26 @@
-package com.Patterns;
-public class PatternFive {
-	public static void main(String[] args) {
-		for(int i=1;i<=5;i++) {
-			if(i%2!=0) {
-				int k=(i-1)*5+1;
-				for(int j=1;j<=5;j++) {
+import java.util.*;
+public class PatternFive 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value ");
+		int n = sc.nextInt();
+		for(int i=1;i<=n;i++) 
+		{
+			if(i%2!=0) 
+			{
+				int k=(i-1)*n+1;
+				for(int j=1;j<=n;j++) 
+				{
 					System.out.print(k++ +"\t");	
 				}
 			}
 			else
 			{
-				int k=i*5;
-				for(int j=1;j<=5;j++) {
+				int k=i*n;
+				for(int j=1;j<=n;j++) 
+				{
 					System.out.print(k-- +"\t");
 				}
 			}
@@ -19,3 +28,16 @@ public class PatternFive {
 		}
 	}
 }
+/*
+Inputs:-
+Enter the value 
+7
+Output:-
+1	2	3	4	5	6	7	
+14	13	12	11	10	9	8	
+15	16	17	18	19	20	21	
+28	27	26	25	24	23	22	
+29	30	31	32	33	34	35	
+42	41	40	39	38	37	36	
+43	44	45	46	47	48	49	
+*/
