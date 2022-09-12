0e8c80949623645cf7c498fd6ecc7a7a834255e0
Nag46214 <97864157+Nag46214@users.noreply.github.com>
2022-09-12 23:07:36 +0530

    Update PatternFour.java

--- PatternFour.java
+++ PatternFour.java
@@ -1,13 +1,31 @@
-package com.Patterns;
-
-public class PatternFour {
-
-	public static void main(String[] args) {
-		for(int i=1;i<=5;i++) {
-			for(int j=5;j>=i;j--) {
+import java.util.*;
+public class PatternFour 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+    		System.out.println("Enter the size ");
+   	 	int n=sc.nextInt();
+		for(int i=1;i<=n;i++) 
+		{
+			for(int j=n;j>=i;j--) 
+			{
 				System.out.print(j);
 			}
 			System.out.println();
 		}
 	}
 }
+/*
+Inputs:-
+Enter the size 
+7
+Output:-
+7 6 5 4 3 2 1 
+7 6 5 4 3 2 
+7 6 5 4 3 
+7 6 5 4 
+7 6 5 
+7 6 
+7 
+*/
