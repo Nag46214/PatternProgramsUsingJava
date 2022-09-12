ed3b818921885be2f72d81aa1f9726b385f77030
Nag46214 <97864157+Nag46214@users.noreply.github.com>
2022-09-12 23:25:44 +0530

    Update PatternSeven.java

--- PatternSeven.java
+++ PatternSeven.java
@@ -1,15 +1,17 @@
-package com.Patterns;
-
+import java.util.*;
 public class PatternSeven {
 
 	public static void main(String[] args) {
-		for(int i=5;i>=1;i--) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value: ");
+		int n=sc.nextInt();
+		for(int i=n;i>=1;i--) {
 			for(int j=i;j>=1;j--) {
 				System.out.print(j+" ");
 			}
 			System.out.println();
 		}
-		for(int i=1;i<=5;i++) {
+		for(int i=1;i<=n;i++) {
 			for(int j=i;j>=1;j--) {
 				System.out.print(j+" ");
 			}
@@ -19,3 +21,25 @@ public class PatternSeven {
 	}
 
 }
+/*
+Inputs:-
+Enter the value: 
+7
+Output:-
+
+7 6 5 4 3 2 1 
+6 5 4 3 2 1 
+5 4 3 2 1 
+4 3 2 1 
+3 2 1 
+2 1 
+1 
+1 
+2 1 
+3 2 1 
+4 3 2 1 
+5 4 3 2 1 
+6 5 4 3 2 1 
+7 6 5 4 3 2 1 
+
+*/
